f56afd423eeb619dbb869e68ff2ace95800841f7
NorCalDavid <mail@daviduli.com>
2016-08-05 17:46:15 -0700

    Added SweetAlert on New Notifications

--- app/assets/javascripts/channels/notifications.js
+++ app/assets/javascripts/channels/notifications.js
@@ -11,11 +11,19 @@ App.notifications = App.cable.subscriptions.create("NotificationsChannel", {
   },
 
   received: function(data) {
-  	console.log(data);
   	$("#notifications").prepend(data.html);
   	$("#notification-icon").html('notifications_active');
   	$("#notification-count").html(data.count);
   	$("#notification-count").show();
+  	successMessage();
   }
 
 });
+
+function successMessage() {
+  sweetAlert({ title: "Success", 
+                html: '<h2>You Have Received a New Notification!</h2>' +
+                    	'<br />',   
+                type: "success",
+              timmer: 2000 });
+};
